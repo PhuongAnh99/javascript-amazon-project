cfd3c99230274aec8eed8bdfc1e80333f4ff21ad
PhuongAnh <phuonganhnthi98@gmail.com>
2025-04-12 16:55:12 +0700

    update Cart method

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 
 let productsHtml = '';
@@ -56,34 +56,20 @@ products.forEach(product => {
     `;
 });
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  cart.forEach(item => {
+      cartQuantity += item.quantity;
+  });
+  document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelector('.products-grid').innerHTML = productsHtml;
 
 document.querySelectorAll('.add-to-cart-button').forEach(button => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-        let matchingItem;
-
-        cart.forEach(item => {
-            if (item.productId === productId) {
-                matchingItem = item;
-            }
-        });
-
-        if (matchingItem) {
-            matchingItem.quantity++;
-        } else {
-            cart.push({
-                productId: productId,
-                quantity: 1
-            });
-        };
-
-        let cartQuantity = 0;
-        cart.forEach(item => {
-            cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.cart-quantity').innerHTML = cartQuantity;
+        addToCart(productId);
+        updateCartQuantity();
     });
 });
