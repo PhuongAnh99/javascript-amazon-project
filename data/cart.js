92e8d0fba48bd9af845aa6351c0314af0fe133ae
PhuongAnh <phuonganhnthi98@gmail.com>
2025-04-12 17:50:30 +0700

    gen HTML for checkout summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [{
+    productId: "54e0eccd-8f36-462b-b68a-8182611d9add",
+    quantity: 2
+}, {
+    productId: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+    quantity: 5
+}, {
+    productId: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+    quantity: 1
+}];
 
 export function addToCart(productId) {
   let matchingItem;
